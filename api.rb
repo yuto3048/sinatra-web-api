8a1385893ebe439b165bc5454b6e5e1ffc497a58
Yuto Fujimura <yuto101024@gmail.com>
2018-07-13 11:06:53 +0900

    add sinatra:   Gemfile new file:   Gemfile.lock edit /show:   api.rb

--- api.rb
+++ api.rb
@@ -8,8 +8,8 @@ get '/show' do
   # Rubyのハッシュを作成、diaryという名前を付ける
   diary = {
       id: 1,
-      title: "今日の日記",
-      content: "きょうは雨でした。"
+      title: "7/13's diary",
+      content: "ねこです"
   }
 
   # diaryをJSONテキストに変換して返す
